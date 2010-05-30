a3ce874fe4d5806b37626e86d1c8d9ea587c0419
David Anderson <dave@natulte.net>
2010-05-30 23:36:37 +0200

    Add documentation to hook evimodeline into an emacs configuration.

--- evimodeline.el
+++ evimodeline.el
@@ -1,13 +1,40 @@
-(defun apply-vim-modeline ()
-  (interactive)
-  (if (evimodeline-has-emacs-modeline)
-      (message "Emacs modeline detected, not applying vim modes")
+;; Copyright (c) 2010 David Anderson <dave@natulte.net>
+;;
+;; Permission is hereby granted, free of charge, to any person
+;; obtaining a copy of this software and associated documentation
+;; files (the "Software"), to deal in the Software without
+;; restriction, including without limitation the rights to use, copy,
+;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;; of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+;;
+;;
+;; Vim modeline support for Emacs. If a loaded file doesn't have Emacs
+;; file-local variable declarations (the -*- ... -*- thing), this
+;; looks for a Vim modeline definition, and applies some of the
+;; whitelisted modes in Emacs terms (mostly things to do with
+;; whitespace handling). To enable, add to your config:
+;;
+;; (add-hook 'find-file-hook 'evimodeline-find-file-hook)
+;;
+
+(defun evimodeline-find-file-hook ()
+  (when (not (evimodeline-has-emacs-modeline))
     (let ((modeline (evimodeline-extract-modeline)))
-      (if (null modeline)
-          (message "no vim modeline found")
-        (progn
-          (message "vim modeline found! %S" modeline)
-          (mapc 'evimodeline-apply-mode modeline))))))
+      (when modeline
+        (mapc 'evimodeline-apply-mode modeline)))))
 
 (defun evimodeline-has-emacs-modeline ()
   (save-excursion
